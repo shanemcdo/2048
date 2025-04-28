55cce8485a2d1951869308cbcc0783225c1e7acb
Shane McDonough <shanemcdman@gmail.com>
2025-04-28 15:30:37 -0700

    re-indent all files

--- index.js
+++ index.js
@@ -5,37 +5,37 @@ const m = new Matrix(() => {
 });
 
 brightness = {
-    0: 100,
-    2: 90,
-    4: 80,
-    8: 70,
-    16: 60,
-    32: 50,
-    64: 40,
-    128: 30,
-    256: 20,
-    512: 10,
-    1024: 5,
-    2048: 0,
+	0: 100,
+	2: 90,
+	4: 80,
+	8: 70,
+	16: 60,
+	32: 50,
+	64: 40,
+	128: 30,
+	256: 20,
+	512: 10,
+	1024: 5,
+	2048: 0,
 }
 
 function get_grid_el(i, j){
-    return grid_els[i * MATSIZE + j];
+	return grid_els[i * MATSIZE + j];
 }
 
 function update_grid(){
-    for(let i = 0; i < MATSIZE; i++){
-        for(let j = 0; j < MATSIZE; j++){
-            let el = get_grid_el(i, j);
-            let val = m.values[i][j]
-            el.innerHTML = val === 0 ? '' : val.toString();
-            el.style.background = `hsl(275, 100%, ${brightness[val]}%)`
-            if(brightness[val] <= 40)
-                el.style.color = 'white';
-            else
-                el.style.color = '';
-        }
-    }
+	for(let i = 0; i < MATSIZE; i++){
+		for(let j = 0; j < MATSIZE; j++){
+			let el = get_grid_el(i, j);
+			let val = m.values[i][j]
+			el.innerHTML = val === 0 ? '' : val.toString();
+			el.style.background = `hsl(275, 100%, ${brightness[val]}%)`
+			if(brightness[val] <= 40)
+				el.style.color = 'white';
+			else
+				el.style.color = '';
+		}
+	}
 }
 
 function reset_game(){
@@ -47,30 +47,30 @@ function reset_game(){
 update_grid();
 
 document.addEventListener('keydown', event=>{
-    switch(event.key){
-        case 'A':
-        case 'a':
-        case 'ArrowLeft':
-            m.move(Direction.Left);
-            break;
-        case 'S':
-        case 's':
-        case 'ArrowDown':
-            m.move(Direction.Down);
-            break;
-        case 'D':
-        case 'd':
-        case 'ArrowRight':
-            m.move(Direction.Right);
-            break;
-        case 'W':
-        case 'w':
-        case 'ArrowUp':
-            m.move(Direction.Up);
-            break;
-        default:
-            return;
-    }
-    m.insert_random();
-    update_grid();
+	switch(event.key){
+		case 'A':
+		case 'a':
+		case 'ArrowLeft':
+			m.move(Direction.Left);
+			break;
+		case 'S':
+		case 's':
+		case 'ArrowDown':
+			m.move(Direction.Down);
+			break;
+		case 'D':
+		case 'd':
+		case 'ArrowRight':
+			m.move(Direction.Right);
+			break;
+		case 'W':
+		case 'w':
+		case 'ArrowUp':
+			m.move(Direction.Up);
+			break;
+		default:
+			return;
+	}
+	m.insert_random();
+	update_grid();
 });
--- matrix.js
+++ matrix.js
@@ -1,213 +1,213 @@
 const MATSIZE = 4;
 class Direction{
-    constructor(name){
-        this.name = name;
-    }
+	constructor(name){
+		this.name = name;
+	}
 
-    static Up = new Direction('Up');
-    static Down = new Direction('Down');
-    static Left = new Direction('Left');
-    static Right = new Direction('Right');
+	static Up = new Direction('Up');
+	static Down = new Direction('Down');
+	static Left = new Direction('Left');
+	static Right = new Direction('Right');
 }
 
 class Matrix{
-    constructor(gameover_callback = () => {}){
+	constructor(gameover_callback = () => {}){
 		this.gameover_callback = gameover_callback;
-        this.values = [];
-        for(let _i = 0; _i < MATSIZE; _i++){
-            let row = [];
-            for(let _i = 0; _i < MATSIZE; _i++)
-                row.push(0);
-            this.values.push(row);
-        }
-        this.insert_random();
+		this.values = [];
+		for(let _i = 0; _i < MATSIZE; _i++){
+			let row = [];
+			for(let _i = 0; _i < MATSIZE; _i++)
+				row.push(0);
+			this.values.push(row);
+		}
+		this.insert_random();
 		this.gameover = false;
-    }
+	}
 
-    slide(direction){
-        let changed = false;
-        switch(direction){
-            case Direction.Right:
-                for(let row of this.values){
-                    for(let i = MATSIZE - 1; i >= 0; i--){
-                        if(row[i] === 0){
-                            let none = true;
-                            for(let j = i - 1; j >= 0; j--){
-                                if(row[j] !== 0){
-                                    row[i] = row[j]
-                                    row[j] = 0;
-                                    none = false;
-                                    changed = true;
-                                    break;
-                                }
-                            }
-                            if(none)
-                                break;
-                        }
-                    }
-                }
-                break;
-            case Direction.Left:
-                for(let row of this.values){
-                    for(let i = 0; i < MATSIZE; i++){
-                        if(row[i] === 0){
-                            let none = true;
-                            for(let j = i; j < MATSIZE; j++){
-                                if(row[j] !== 0){
-                                    row[i] = row[j]
-                                    row[j] = 0;
-                                    none = false;
-                                    changed = true;
-                                    break;
-                                }
-                            }
-                            if(none)
-                                break;
-                        }
-                    }
-                }
-                break;
-            case Direction.Up:
-                for(let col = 0; col < MATSIZE; col++){
-                    for(let i = 0; i < MATSIZE; i++){
-                        if(this.values[i][col] === 0){
-                            let none = true;
-                            for(let j = i; j < MATSIZE; j++){
-                                if(this.values[j][col] !== 0){
-                                    this.values[i][col] = this.values[j][col]
-                                    this.values[j][col] = 0;
-                                    none = false;
-                                    changed = true;
-                                    break;
-                                }
-                            }
-                            if(none)
-                                break;
-                        }
-                    }
-                }
-                break;
-            case Direction.Down:
-                for(let col = 0; col < MATSIZE; col++){
-                    for(let i = MATSIZE - 1; i >= 0 ; i--){
-                        if(this.values[i][col] === 0){
-                            let none = true;
-                            for(let j = i - 1; j >= 0; j--){
-                                if(this.values[j][col] !== 0){
-                                    this.values[i][col] = this.values[j][col]
-                                    this.values[j][col] = 0;
-                                    none = false;
-                                    changed = true;
-                                    break;
-                                }
-                            }
-                            if(none)
-                                break;
-                        }
-                    }
-                }
-                break;
-            default:
-        }
-        return changed
-    }
+	slide(direction){
+		let changed = false;
+		switch(direction){
+			case Direction.Right:
+				for(let row of this.values){
+					for(let i = MATSIZE - 1; i >= 0; i--){
+						if(row[i] === 0){
+							let none = true;
+							for(let j = i - 1; j >= 0; j--){
+								if(row[j] !== 0){
+									row[i] = row[j]
+									row[j] = 0;
+									none = false;
+									changed = true;
+									break;
+								}
+							}
+							if(none)
+								break;
+						}
+					}
+				}
+				break;
+			case Direction.Left:
+				for(let row of this.values){
+					for(let i = 0; i < MATSIZE; i++){
+						if(row[i] === 0){
+							let none = true;
+							for(let j = i; j < MATSIZE; j++){
+								if(row[j] !== 0){
+									row[i] = row[j]
+									row[j] = 0;
+									none = false;
+									changed = true;
+									break;
+								}
+							}
+							if(none)
+								break;
+						}
+					}
+				}
+				break;
+			case Direction.Up:
+				for(let col = 0; col < MATSIZE; col++){
+					for(let i = 0; i < MATSIZE; i++){
+						if(this.values[i][col] === 0){
+							let none = true;
+							for(let j = i; j < MATSIZE; j++){
+								if(this.values[j][col] !== 0){
+									this.values[i][col] = this.values[j][col]
+									this.values[j][col] = 0;
+									none = false;
+									changed = true;
+									break;
+								}
+							}
+							if(none)
+								break;
+						}
+					}
+				}
+				break;
+			case Direction.Down:
+				for(let col = 0; col < MATSIZE; col++){
+					for(let i = MATSIZE - 1; i >= 0 ; i--){
+						if(this.values[i][col] === 0){
+							let none = true;
+							for(let j = i - 1; j >= 0; j--){
+								if(this.values[j][col] !== 0){
+									this.values[i][col] = this.values[j][col]
+									this.values[j][col] = 0;
+									none = false;
+									changed = true;
+									break;
+								}
+							}
+							if(none)
+								break;
+						}
+					}
+				}
+				break;
+			default:
+		}
+		return changed
+	}
 
-    merge(direction){
-        let changed = false;
-        switch(direction){
-            case Direction.Right:
-                for(let row of this.values){
-                    for(let i = MATSIZE - 2; i >= 0; i--){
-                        if(row[i] === row[i + 1]){
-                            row[i + 1] = 0;
-                            row[i] *= 2;
-                            changed = true;
-                        }
-                    }
-                }
-                break;
-            case Direction.Left:
-                for(let row of this.values){
-                    for(let i = 0; i < MATSIZE - 1; i++){
-                        if(row[i] === row[i + 1]){
-                            row[i] = 0;
-                            row[i + 1] *= 2;
-                            changed = true;
-                        }
-                    }
-                }
-                break;
-            case Direction.Up:
-                for(let col = 0; col < MATSIZE; col++){
-                    for(let i = 0; i < MATSIZE - 1; i++){
-                        if(this.values[i][col] === this.values[i + 1][col]){
-                            this.values[i + 1][col] = 0;
-                            this.values[i][col] *= 2;
-                            changed = true;
-                        }
-                    }
-                }
-                break;
-            case Direction.Down:
-                for(let col = 0; col < MATSIZE; col++){
-                    for(let i = MATSIZE - 2; i >= 0 ; i--){
-                        if(this.values[i][col] === this.values[i + 1][col]){
-                            this.values[i + 1][col] = 0;
-                            this.values[i][col] *= 2;
-                            changed = true;
-                        }
-                    }
-                }
-                break;
-            default:
-        }
-        return changed
-    }
+	merge(direction){
+		let changed = false;
+		switch(direction){
+			case Direction.Right:
+				for(let row of this.values){
+					for(let i = MATSIZE - 2; i >= 0; i--){
+						if(row[i] === row[i + 1]){
+							row[i + 1] = 0;
+							row[i] *= 2;
+							changed = true;
+						}
+					}
+				}
+				break;
+			case Direction.Left:
+				for(let row of this.values){
+					for(let i = 0; i < MATSIZE - 1; i++){
+						if(row[i] === row[i + 1]){
+							row[i] = 0;
+							row[i + 1] *= 2;
+							changed = true;
+						}
+					}
+				}
+				break;
+			case Direction.Up:
+				for(let col = 0; col < MATSIZE; col++){
+					for(let i = 0; i < MATSIZE - 1; i++){
+						if(this.values[i][col] === this.values[i + 1][col]){
+							this.values[i + 1][col] = 0;
+							this.values[i][col] *= 2;
+							changed = true;
+						}
+					}
+				}
+				break;
+			case Direction.Down:
+				for(let col = 0; col < MATSIZE; col++){
+					for(let i = MATSIZE - 2; i >= 0 ; i--){
+						if(this.values[i][col] === this.values[i + 1][col]){
+							this.values[i + 1][col] = 0;
+							this.values[i][col] *= 2;
+							changed = true;
+						}
+					}
+				}
+				break;
+			default:
+		}
+		return changed
+	}
 
-    move(direction){
-        this.slide(direction);
-        this.merge(direction);
-        this.slide(direction);
-        this.log()
+	move(direction){
+		this.slide(direction);
+		this.merge(direction);
+		this.slide(direction);
+		this.log()
 		this.check_gameover();
-    }
+	}
 
-    log(){
-        for(const row of this.values)
-            console.log(row);
-    }
+	log(){
+		for(const row of this.values)
+			console.log(row);
+	}
 
-    random_number(){
-        let rand = Math.random();
-        if(rand <= 0.6){
-            return 2
-        }else if(rand <= 0.9){
-            return 4
-        }else if(rand <= 0.96){
-            return 8
-        }
-        return 16
-    }
+	random_number(){
+		let rand = Math.random();
+		if(rand <= 0.6){
+			return 2
+		}else if(rand <= 0.9){
+			return 4
+		}else if(rand <= 0.96){
+			return 8
+		}
+		return 16
+	}
 
-    free_spaces(){
-        let spaces = [];
-        for(let i = 0; i < MATSIZE; i++){
-            for(let j = 0; j < MATSIZE; j++){
-                if(this.values[i][j] === 0)
-                    spaces.push({'i': i, 'j': j});
-            }
-        }
-        return spaces;
-    }
+	free_spaces(){
+		let spaces = [];
+		for(let i = 0; i < MATSIZE; i++){
+			for(let j = 0; j < MATSIZE; j++){
+				if(this.values[i][j] === 0)
+					spaces.push({'i': i, 'j': j});
+			}
+		}
+		return spaces;
+	}
 
-    insert_random(){
-        let num = this.random_number();
-        let spaces = this.free_spaces();
+	insert_random(){
+		let num = this.random_number();
+		let spaces = this.free_spaces();
 		if(spaces.length > 0) {
 			let space = spaces[Math.floor(Math.random() * spaces.length)]
 			this.values[space.i][space.j] = num;
 		}
-    }
+	}
 
 	check_gameover(){
 		this.gameover = true;
@@ -241,12 +241,12 @@ class Matrix{
 	}
 
 	reset(){
-        for(let i = 0; i < MATSIZE; i++){
-            for(let j = 0; j < MATSIZE; j++){
+		for(let i = 0; i < MATSIZE; i++){
+			for(let j = 0; j < MATSIZE; j++){
 				this.values[i][j] = 0;
 			}
-        }
-        this.insert_random();
+		}
+		this.insert_random();
 		this.gameover = false;
 	}
 }
